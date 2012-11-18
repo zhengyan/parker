41250ef2113892e71fbe99a60288ce9604848d74
Andy Zheng <hzzhengyan@yahoo.com.cn>
2012-11-18 15:56:07 +0800

    Parker update 20121118

---
+++ src/Parking/Car.java
@@ -0,0 +1,12 @@
+package Parking;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: software
+ * Date: 12-11-11
+ * Time: 下午3:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class Car {
+
+}
