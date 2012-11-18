9dfd208f0113b00dbf71a65c17d8925e36e6a90c
Andy Zheng <hzzhengyan@yahoo.com.cn>
2012-11-18 16:45:50 +0800

    Parker update 20121118_2

---
+++ src/Parking/Ticket.java
@@ -0,0 +1,11 @@
+package Parking;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: software
+ * Date: 12-11-18
+ * Time: 下午4:18
+ * To change this template use File | Settings | File Templates.
+ */
+public class Ticket {
+}
