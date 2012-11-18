b95f8649cbe3a14cc2e0999afbf92e4ee7b18312
Andy Zheng <hzzhengyan@yahoo.com.cn>
2012-11-18 15:44:13 +0800

    Parker Update

---
+++ Parker/src/Parking/UnknownTID.java
@@ -0,0 +1,11 @@
+package Parking;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: software
+ * Date: 12-11-11
+ * Time: 下午4:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class UnknownTID extends RuntimeException {
+}
